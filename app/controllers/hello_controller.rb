c08ddab7a544dbf11ad2d3234e9c3347e7499df6
Justin McKee <justinmckee@Justins-MacBook-Pro.local>
2018-06-04 18:16:38 -0700

    First commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,8 @@
+class HelloController < ApplicationController
+  def index
+  end
+
+  def sample
+    @controller_message = "I'm an interactive developer who wants to focus on building award-winning web applications in Angular, Rails, React, or whatever framework works for your team!"
+  end
+end
